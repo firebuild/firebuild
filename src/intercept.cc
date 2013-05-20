c2fff02bba4fbdac548a1ba852e05ff831b647f9
Balint Reczey <balint@balintreczey.hu>
2013-05-19 20:56:33 -0500

    add intercept_open()

--- src/intercept.cc
+++ src/intercept.cc
@@ -9,6 +9,9 @@
 #include "env.h"
 #include "fb-messages.pb.h"
 
+/* needs to be included after protobuf messages due to errno macro definition */
+#include <errno.h>
+
 using namespace std;
 
 #ifdef  __cplusplus
@@ -93,6 +96,22 @@ static void fb_ic_load()
 // TODO? 
 //int fcntl (int __fd, int __cmd, ...);
 
+/* */
+static void
+intercept_open (const char *file, const int flags, const int mode,
+	       const int ret, const int open_errno)
+{
+  OpenFile m;
+  m.set_pid(getpid());
+  m.set_file(file);
+  m.set_flags(flags);
+  m.set_mode(mode);
+  m.set_errno(open_errno);
+
+  cout << "intercept open!" << endl;
+  // TODO send to supervisor and collect file status if needed
+}
+
 /**
  * Intercept open variants with varible length arg list.
  * mode is filled based on presence of O_CREAT flag
@@ -100,6 +119,7 @@ static void fb_ic_load()
 #define IC_OPEN_VA(ret_type, name, parameters, body)			\
   IC(ret_type, name, parameters,					\
      {									\
+       int open_errno;							\
        mode_t mode = 0;							\
        if (__oflag & O_CREAT) {						\
 	 va_list ap;							\
@@ -107,34 +127,25 @@ static void fb_ic_load()
 	 mode = va_arg(ap, mode_t);					\
 	 va_end(ap);							\
        }								\
-       									\
+									\
        body;								\
+       open_errno = errno;						\
+       intercept_open(__file, __oflag, mode, ret, open_errno);		\
+       errno = open_errno;						\
      })
 
 
 IC_OPEN_VA(int, open, (__const char *__file, int __oflag, ...),
-	   {
-	     cout << "intercept!" << endl;
-	     ret = orig_fn(__file, __oflag, mode);
-	   })
+	   {ret = orig_fn(__file, __oflag, mode);})
 
 IC_OPEN_VA(int, open64, (__const char *__file, int __oflag, ...),
-	   {
-	     cout << "intercept!" << endl;
-	     ret = orig_fn(__file, __oflag, mode);
-	   })
+	   {ret = orig_fn(__file, __oflag, mode);})
 
-IC_OPEN_VA(int, openat, (int __fd, int __oflag, ...),
-	   {
-	     cout << "intercept!" << endl;
-	     ret = orig_fn(__fd, __oflag, mode);
-	   })
+IC_OPEN_VA(int, openat, (int __fd, __const char *__file, int __oflag, ...),
+	   {ret = orig_fn(__fd, __file, __oflag, mode);})
 
-IC_OPEN_VA(int, openat64, (int __fd, int __oflag, ...),
-	   {
-	     cout << "intercept!" << endl;
-	     ret = orig_fn(__fd, __oflag, mode);
-	   })
+IC_OPEN_VA(int, openat64, (int __fd, __const char *__file, int __oflag, ...),
+	   {ret = orig_fn(__fd, __file, __oflag, mode);})
 
 IC(int, creat, (__const char *__file, __mode_t __mode),
 	   {
