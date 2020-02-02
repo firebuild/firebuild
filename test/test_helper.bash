63fb96b2bc84b6b8dd74799e3f606dae2a25a7f9
Balint Reczey <balint@balintreczey.hu>
2020-02-02 20:08:39 +0100

    test/test_helper.bash: Use realpath
    This allows ./run-firebuild make -C ... to work

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=../src/interceptor
-export PATH=../src/firebuild:$PATH
-export FIREBUILD_CACHE_DIR=./test_cache_dir
+export LD_LIBRARY_PATH=$(realpath ../src/interceptor)
+export PATH=$(realpath ../src/firebuild):$PATH
+export FIREBUILD_CACHE_DIR=$(realpath ./test_cache_dir)
