ab7f0e69cdcaf890908a479a41333dcbe87ca6b4
Egmont Koblinger <egmont@gmail.com>
2020-02-23 13:32:26 +0100

    gcov: Reorganize tree
    Make sure that tests measured with gcov place their *.gcda files under
    the gcov directory. Do all the intermediate computations here too.
    
    Place the coverage report under the gcovhtml directory.

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -2,6 +2,8 @@
 export LD_LIBRARY_PATH=$(realpath ../src/interceptor)
 export PATH=$(realpath ../src/firebuild):$PATH
 export FIREBUILD_CACHE_DIR=$(realpath ./test_cache_dir)
+export GCOV_PREFIX=$(realpath ..)/gcov
+export GCOV_PREFIX_STRIP=$(realpath .. | tr -dc / | wc -c)
 
 function strip_stderr () {
     awk '/^==[0-9]*== $/ {next} /FILE DESCRIPTORS: 0 open at exit/ {next} {print}' < $1
