1c03f7b058f1a31ef1ce4fd0edc58e72469517c3
Balint Reczey <balint@balintreczey.hu>
2020-05-10 19:44:53 +0200

    Leave supervisor connection open in interceptor's exit handler
    This lets interception of other exit handlers.

--- src/interceptor/intercept.cc
+++ src/interceptor/intercept.cc
@@ -285,13 +285,10 @@ void on_exit_handler(const int status, void *) {
   handle_exit(status);
   insert_debug_msg("our_on_exit_handler-end");
 
-  /* Destruct global stuff here so that valgrind doesn't complain.
-   * This is the last place anything from the interceptor code can be
-   * executed. Alas it's skipped if one does an _exit(). */
-  google::protobuf::ShutdownProtobufLibrary();
-  ic_orig_close(fb_sv_conn);
-  free(fb_conn_string);
-  delete fd_states;
+  /* Destruction of global objects is not done here, because other exit handlers
+   * may perform actions that need to be reported to the supervisor.
+   * TODO(rbalint) add Valgrind suppress file
+   */
 }
 
 void handle_exit(const int status) {
