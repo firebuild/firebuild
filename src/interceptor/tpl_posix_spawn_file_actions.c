f1cc189d24b1e12a247c4bf7c3c6050bbc3a7a77
Egmont Koblinger <egmont@gmail.com>
2020-09-06 00:28:58 +0200

    interceptor: More robust guarding of the communication file descriptor
    Fixes #267

--- src/interceptor/tpl_posix_spawn_file_actions.c
+++ src/interceptor/tpl_posix_spawn_file_actions.c
@@ -6,6 +6,17 @@
 {# ------------------------------------------------------------------ #}
 ### extends "tpl.c"
 
+### block guard_connection_fd
+###   for (type, name) in types_and_names
+{# It is ugly to check for the variable name to end with "fd", but is simple works well in practice. #}
+###     if type == "int" and name[-2:] == "fd"
+{# Unlike most methods which place the error code in errno and return -1, #}
+{# posix_spawn_file_actions_add*() return the error code directly.        #}
+{# FIXME Do we want to return an error here, or maybe pretend success?    #}
+  if ({{ name }} == fb_sv_conn) return EBADF;
+###     endif
+###   endfor
+### endblock
 ### block after
   if (success) {
     {{ func | replace("posix_spawn_file_actions_", "psfa_") }} ({{ names_str }});
