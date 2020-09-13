2f7d5bfe87e4dfc0bcfe7f14afca1b8acd7f5cd2
Egmont Koblinger <egmont@gmail.com>
2020-09-13 21:37:00 +0200

    interceptor: Do not lock for read and write operations
    Lock for the communication, though.
    
    Fixes #278, fixes #279

--- src/interceptor/tpl_read.c
+++ src/interceptor/tpl_read.c
@@ -7,11 +7,22 @@
 ### extends "tpl.c"
 
 {% set msg = "read" %}
-{% set send_msg_condition = "(fd < 0 || fd >= IC_FD_STATES_SIZE || ic_fd_states[fd].read == false)" %}
+{# No locking around the read(): see issue #279 #}
+{% set global_lock = False %}
 
 ### block send_msg
-  {{ super() }}
-  if (fd >= 0 && fd < IC_FD_STATES_SIZE) {
-    ic_fd_states[fd].read = true;
+  {# Acquire the lock if sending a message #}
+  if (fd < 0 || fd >= IC_FD_STATES_SIZE || ic_fd_states[fd].read == false) {
+    /* Need to notify the supervisor */
+
+    {{ grab_lock_if_needed('true') | indent(2) }}
+
+    {{ super() | indent(2) }}
+
+    if (fd >= 0 && fd < IC_FD_STATES_SIZE) {
+      ic_fd_states[fd].read = true;
+    }
+
+    {{ release_lock_if_needed() | indent(2) }}
   }
 ### endblock send_msg
