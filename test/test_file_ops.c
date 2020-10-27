5086d84bf2e955ff54d26cf1410191b638f5a345
Balint Reczey <balint@balintreczey.hu>
2020-10-27 16:51:38 +0100

    test: Cover dup() variants

--- test/test_file_ops.c
+++ test/test_file_ops.c
@@ -1,6 +1,7 @@
 /* Copyright (c) 2020 Interri Kft. */
 /* This file is an unpublished work. All rights reserved. */
 
+#define _GNU_SOURCE
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,12 +15,27 @@
 #define LOC "[" __FILE__ ":" TOSTR(__LINE__) "]"
 
 int main() {
-  int fd, i;
+  int fd, fd_dup, fd_dup2, fd_dup3, i;
 
   /* Set up some files for test_file_ops_[23]. */
   fd = creat("test_empty_1.txt", 0600);
   if (fd == -1) {
     perror("open" LOC);
+    exit(1);
+  }
+  fd_dup = dup(fd);
+  if (fd_dup == -1) {
+    perror("dup" LOC);
+    exit(1);
+  }
+  fd_dup2 = dup2(fd, fd_dup);
+  if (fd_dup2 == -1) {
+    perror("dup2" LOC);
+    exit(1);
+  }
+  fd_dup3 = dup3(fd, fd_dup2, O_CLOEXEC);
+  if (fd_dup3 == -1) {
+    perror("dup3" LOC);
     exit(1);
   }
   close(fd);
