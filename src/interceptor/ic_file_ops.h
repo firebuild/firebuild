b8cbb9dfb778cc48e9a621dd599a80f8062fef9d
Egmont Koblinger <egmont@gmail.com>
2021-10-13 12:20:57 +0200

    firebuild: Don't notify the supervisor on read/write to non-inherited fds
    Fixes #557

--- src/interceptor/ic_file_ops.h
+++ src/interceptor/ic_file_ops.h
@@ -13,6 +13,7 @@
 
 int intercept_fopen_mode_to_open_flags_helper(const char * mode);
 int popen_type_to_flags(const char * type);
+void set_file_state(const int fd);
 void clear_file_state(const int fd);
 void clear_all_file_states();
 void copy_file_state(const int to_fd, const int from_fd);
