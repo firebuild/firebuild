firebuild/firebuild

commit 0d81e68698a80e1122626a21bae7d74b022b353d

supervisor: Refactor file usage handling and updating
Fixes #741.

src/firebuild/execed_process.cc
@@ -98,10 +98,11 @@ void ExecedProcess::initialize() {
   /* Propagate the opening of the executable and libraries upwards as
    * regular file open events. */
   if (parent_exec_point()) {
-    parent_exec_point()->register_file_usage(executable(), executable(),
-                                             FILE_ACTION_OPEN, O_RDONLY, 0);
+    FileUsageUpdate exe_update = FileUsageUpdate::get_from_open_params(executable(), O_RDONLY, 0);
+    parent_exec_point()->register_file_usage_update(executable(), exe_update);
     for (const auto& lib : libs_) {
-      parent_exec_point()->register_file_usage(lib, lib, FILE_ACTION_OPEN, O_RDONLY, 0);
+      FileUsageUpdate lib_update = FileUsageUpdate::get_from_open_params(lib, O_RDONLY, 0);
+      parent_exec_point()->register_file_usage_update(lib, lib_update);
     }
   }
 
@@ -213,223 +214,108 @@ void ExecedProcess::do_finalize() {
 }
 
 /**
- * This is called on file operation methods, for all the ExecedProcess
- * ancestors, excluding the exec_point where the file operation
- * occurred. The method does the necessary administration, and bubbles
- * it upwards to the root.
+ * Registers a file operation described in "update" into the filename "name", and bubbles it up to
+ * the root.
  *
- * In case of non-shortcutted processes, in the exec_point the method
- * register_file_usage() performed the necessary administration before
- * beginning to bubble up this event.
+ * "update" might contain some lazy bits that will be computed on demand.
  *
- * In case of shortcutted processes, it's the shortcutting itself that
- * performs the file operations, no administration is necessary there.
+ * In some rare cases the filename within "update" might differ from "name", in that case the
+ * filename mentioned in "update" is used to lazily figure out the required values (such as
+ * checksum), but it is registered as if it belonged to the file mentioned in this method's "name"
+ * parameter. Currently this trick is only used for a rename()'s source path.
+ *
+ * This method also registers the implicit parent directory and bubbles it up, as per the
+ * information contained in "update".
  */
-void ExecedProcess::propagate_file_usage(const FileName *name,
-                                         const FileUsage* fu_change) {
-  TRACKX(FB_DEBUG_PROC, 1, 1, Process, this, "name=%s, fu_change=%s", D(name), D(fu_change));
+bool ExecedProcess::register_file_usage_update(const FileName *name,
+                                        const FileUsageUpdate& update) {
+  TRACKX(FB_DEBUG_PROC, 1, 1, Process, this, "name=%s, update=%s", D(name), D(update));
 
-  const FileUsage *fu;
-  bool propagate = false;
-  auto it = file_usages_.find(name);
-  if (it != file_usages_.end()) {
-    fu = it->second;
-    const FileUsage* merged_fu = fu->merge(fu_change);
-    if (!merged_fu) {
-      disable_shortcutting_bubble_up("Could not register unsupported file usage combination");
-      return;
-    }
-    if (merged_fu != fu) {
-      it.value() = fu = merged_fu;
-      propagate = true;
-    }
-  } else {
-    file_usages_[name] = fu = fu_change;
-    propagate = true;
-  }
-
-  /* Propagage change further if needed. */
-  if (propagate) {
-    ExecedProcess* next = generate_report ? parent_exec_point() : next_shortcutable_ancestor();
-    if (next) {
-      next->propagate_file_usage(name, fu);
-    }
-  }
-}
-
-/**
- * This is called on the exec_point of a non-shortcutted process when an
- * open() or similar call is intercepted. Converts the input into a
- * FileUsage, stat'ing the file, computing its checksum if necessary.
- * Registers the file operation, and bubbles it upwards to the root via
- * propagate_file_usage().
- * Looks at the contents of `actual_file`, but registers as if the event
- * happened to `name`.
- */
-bool ExecedProcess::register_file_usage(const FileName *name,
-                                        const FileName *actual_file,
-                                        FileAction action,
-                                        int flags,
-                                        int error) {
-  TRACKX(FB_DEBUG_PROC, 1, 1, Process, this, "name=%s, actual_file=%s, flags=%d, error=%d",
-         D(name), D(actual_file), flags, error);
+  /* What the FileUsage was before the update, on the previous (descendant) level. The initial value
+   * should differ from any valid value, including NULL which is valid too (indicating no prior
+   * knowledge about that file), to allow to detect if we're about the perform the same update
+   * operation as we did on the previous (descendant) level. It should also differ from fu_new's
+   * initial value, to allow to detect if the update didn't change anything. */
+  const FileUsage *fu_old = reinterpret_cast<FileUsage *>(-1);
+  /* What the FileUsage became after the update, on the previous (descendant) level. */
+  const FileUsage *fu_new = nullptr;
 
   if (name->is_in_ignore_location()) {
     FB_DEBUG(FB_DEBUG_FS, "Ignoring file usage: " + d(name));
     return true;
   }
 
-  if (!can_shortcut_ && !generate_report) {
+  ExecedProcess *proc = this;
+  if (!proc->can_shortcut_ && !generate_report) {
     /* Register at the first shortcutable ancestor instead. */
-    ExecedProcess* next = next_shortcutable_ancestor();
-    if (next) {
-      return next->register_file_usage(name, actual_file, action, flags, error);
-    } else {
-      return true;
-    }
+    proc = proc->next_shortcutable_ancestor();
   }
-
-  if (error) {
-    switch (error) {
-      case ENOENT: {
-        if (is_write(flags)) {
-          /* When opening a file for writing the absence of the parent dir
-           * results NOTEXIST error. The grandparent dir could be missing as well,
-           * but the missing parent dir would cause the same error thus it will not be a mistake
-           * to shortcut the process if the parent dir is indeed missing.
-           */
-          return register_parent_directory(actual_file, NOTEXIST);
-        }
-        break;
-      }
-      case ENOTDIR: {
-        /* Occurs when opening the "foo/baz/bar" path when "foo/baz" is not a directory,
-         * but for example a regular file. Or when "foo" is a regular file. We can't distinguish
-         * between those cases, but if "/foo/baz" is a regular file we can safely shortcut the
-         * process, because the process could not tell the difference either. */
-        return register_parent_directory(actual_file, ISREG);
-      }
-      default:
-        break;
-        /* To be handled later. */
-    }
-  }
-
-  const FileUsage *fu = nullptr;
-  auto it = file_usages_.find(name);
-  if (it != file_usages_.end()) {
-    fu = it->second;
-  }
-  if (fu) {
-    /* The process already used this file. The initial state was already
-     * recorded, but it can be extended. We obtain a new FileUsage object which represents the
-     * modifications to apply currently, and then we propagate this upwards to be applied.
-     */
-    /* If hash is not stored yet it may need to be. */
-    bool read_again = !fu->written() && !fu->initial_hash_known();
-    const FileUsage *fu_change = FileUsage::Get(actual_file, action, flags, error, read_again);
-    if (!fu_change) {
-      /* Error */
-      return false;
-    } else if (fu_change == fu) {
-      /* This file usage is already registered identically for this file and is also propoagated */
-      return true;
-    }
-    const FileUsage* merged_fu = fu->merge(fu_change);
-    if (!merged_fu) {
-      disable_shortcutting_bubble_up("Could not register unsupported file usage combination");
-      return false;
-    }
-    if (merged_fu != fu) {
-      it.value() = merged_fu;
-      if (parent_exec_point()) {
-        parent_exec_point()->propagate_file_usage(name, merged_fu);
-      }
-    }
-  } else {
-    /* The process opens this file for the first time. Compute whatever
-     * we need to know about its initial state. Use that same object to
-     * propagate the changes upwards. */
-    fu = FileUsage::Get(actual_file, action, flags, error, true);
-    if (!fu) {
-      /* Error */
-      return false;
-    }
-    file_usages_[name] = fu;
-    if (parent_exec_point()) {
-      parent_exec_point()->propagate_file_usage(name, fu);
-    }
-  }
-  return true;
-}
-
-/**
- * See the other register_file_usage().
- * This one does not look at the file system, but instead registers the given fu_change.
- */
-bool ExecedProcess::register_file_usage(const FileName *name,
-                                        const FileUsage* fu_change) {
-  TRACKX(FB_DEBUG_PROC, 1, 1, Process, this, "name=%s, fu_change=%s", D(name), D(fu_change));
-
-  if (!can_shortcut_ && !generate_report) {
-    /* Register at the first shortcutable ancestor instead. */
-    ExecedProcess* next = next_shortcutable_ancestor();
-    if (next) {
-      return next->register_file_usage(name, fu_change);
-    } else {
-      return true;
-    }
-  }
-
-  if (name->is_in_ignore_location()) {
-    FB_DEBUG(FB_DEBUG_FS, "Ignoring file usage: " + d(name));
+  if (!proc) {
     return true;
   }
 
-  const FileUsage *fu = nullptr;
-  auto it = file_usages_.find(name);
-  if (it != file_usages_.end()) {
-    fu = it->second;
+  /* Register (and bubble up) what we implicitly got to know about the parent directory. This call
+   * will in turn call back to us, but it's not a recursion since 'update' will then have
+   * parent_type_ == DONTKNOW. */
+  if (update.parent_type() != DONTKNOW) {
+    proc->register_parent_directory(name, update.parent_type());
   }
-  if (fu) {
-    const FileUsage* merged_fu = fu->merge(fu_change);
-    if (!merged_fu) {
-      disable_shortcutting_bubble_up("Could not register unsupported file usage combination");
-      return false;
+
+  /* Bubble up to the root, but abort the loop if at a certain level we didn't do anything (fu_new
+   * became the same as fu_old was) because then continuing to bubble up wouldn't do anything new
+   * either. */
+  while (proc && fu_new != fu_old) {
+    const FileUsage *fu = nullptr;
+
+    auto it = proc->file_usages_.find(name);
+    if (it != proc->file_usages_.end()) {
+      fu = it->second;
     }
-    if (merged_fu == fu) {
-      return true;
+
+    if (fu == fu_old) {
+      /* Quick path: We need to do the same as we did on the previous level (fu and fu_old can be
+       * NULL or non-NULL, the same quick path logic applies). The new value is fu_new (which can be
+       * the same as fu_old or can differ, again, the same quick path logic applies for both cases),
+       * just as it was on the previous level. Nothing to do here. */
     } else {
-      it.value() = fu = merged_fu;
+      /* Need to merge "update" into "fu". */
+      fu_old = fu;
+      if (!fu) {
+        fu = FileUsage::Get(DONTKNOW);
+      }
+      /* Note: This can update "update" if some value is computed now and cached there. In that
+       * case, in the next iteration of the loop "update" will already contain this value. */
+      fu_new = fu->merge(update);
+      if (!fu_new) {
+        disable_shortcutting_bubble_up("Could not register unsupported file usage combination");
+        return false;
+      }
     }
-  } else {
-    file_usages_[name] = fu = fu_change;
-  }
-  if (parent_exec_point()) {
-    parent_exec_point()->propagate_file_usage(name, fu);
+
+    if (it != proc->file_usages_.end()) {
+      /* Update the previous value. */
+      it.value() = fu_new;
+    } else {
+      /* Insert a new value. */
+      proc->file_usages_[name] = fu_new;
+    }
+
+    if (!generate_report) {
+      proc = proc->next_shortcutable_ancestor();
+    } else {
+      proc = proc->exec_point();
+    }
   }
   return true;
 }
 
 /**
- * Register that the parent (a.k.a. dirname) of the given path does (or does not) exist
- * and is a directory, and bubbles it upwards to the root. See #259 for rationale.
- * To be called on the exec_point of a non-shortcutted process when something is done to
- * the given file.
+ * Register that the parent (a.k.a. dirname) of the given path does (or does not) exist and is of
+ * the given "type" (e.g. ISDIR, NOTEXIST), and bubbles it up to the root.
  */
 bool ExecedProcess::register_parent_directory(const FileName *name,
-                                              FileInitialState initial_state) {
+                                              FileType type) {
   TRACKX(FB_DEBUG_PROC, 1, 1, Process, this, "name=%s", D(name));
-
-  if (!can_shortcut_ && !generate_report) {
-    /* Register at the first shortcutable ancestor instead. */
-    ExecedProcess* next = next_shortcutable_ancestor();
-    if (next) {
-      return next->register_parent_directory(name);
-    } else {
-      return true;
-    }
-  }
 
   const FileName* const parent_dir = FileName::GetParentDir(name->c_str(), name->length());
 
@@ -440,7 +326,9 @@ bool ExecedProcess::register_parent_directory(const FileName *name,
     return true;
   }
 
-  return register_file_usage(parent_dir, FileUsage::Get(initial_state));
+  FileUsageUpdate update(parent_dir, type);
+  assert(update.parent_type() == DONTKNOW);
+  return register_file_usage_update(parent_dir, update);
 }
 
 /* Find and apply shortcut */
@@ -638,7 +526,7 @@ void ExecedProcess::export2js(const unsigned int level,
 
   fprintf(stream, "%s fcreated: [", indent);
   for (auto& ffu : ordered_file_usages) {
-    bool isreg_with_hash = ffu.usage->initial_state() == ISREG && ffu.usage->initial_hash_known();
+    bool isreg_with_hash = ffu.usage->initial_type() == ISREG && ffu.usage->initial_hash_known();
     if (!isreg_with_hash && ffu.usage->written()) {
       fprintf(stream, "\"%s\",", ffu.file->c_str());
     }
@@ -647,7 +535,7 @@ void ExecedProcess::export2js(const unsigned int level,
 
   fprintf(stream, "%s fmodified: [", indent);
   for (auto& ffu : ordered_file_usages) {
-    bool isreg_with_hash = ffu.usage->initial_state() == ISREG && ffu.usage->initial_hash_known();
+    bool isreg_with_hash = ffu.usage->initial_type() == ISREG && ffu.usage->initial_hash_known();
     if (isreg_with_hash && ffu.usage->written()) {
       fprintf(stream, "\"%s\",", ffu.file->c_str());
     }
@@ -656,7 +544,7 @@ void ExecedProcess::export2js(const unsigned int level,
 
   fprintf(stream, "%s fread: [", indent);
   for (auto& ffu : ordered_file_usages) {
-    bool isreg_with_hash = ffu.usage->initial_state() == ISREG && ffu.usage->initial_hash_known();
+    bool isreg_with_hash = ffu.usage->initial_type() == ISREG && ffu.usage->initial_hash_known();
     if (isreg_with_hash && !ffu.usage->written()) {
       fprintf(stream, "\"%s\",", ffu.file->c_str());
     }
@@ -665,7 +553,7 @@ void ExecedProcess::export2js(const unsigned int level,
 
   fprintf(stream, "%s fnotf: [", indent);
   for (auto& ffu : ordered_file_usages) {
-    bool isreg_with_hash = ffu.usage->initial_state() == ISREG && ffu.usage->initial_hash_known();
+    bool isreg_with_hash = ffu.usage->initial_type() == ISREG && ffu.usage->initial_hash_known();
     if (!isreg_with_hash && !ffu.usage->written()) {
       fprintf(stream, "\"%s\",", ffu.file->c_str());
     }

src/firebuild/CMakeLists.txt
@@ -63,7 +63,9 @@ add_executable(firebuild-bin
   hash_cache.cc
   file.cc
   file_fd.cc
+  file_info.cc
   file_usage.cc
+  file_usage_update.cc
   blob_cache.cc
   obj_cache.cc
   utils.cc

src/firebuild/execed_process.h
@@ -14,8 +14,10 @@
 #include <string>
 #include <vector>
 
+#include "firebuild/file_info.h"
 #include "firebuild/file_name.h"
 #include "firebuild/file_usage.h"
+#include "firebuild/file_usage_update.h"
 #include "firebuild/pipe.h"
 #include "firebuild/pipe_recorder.h"
 #include "firebuild/process.h"
@@ -95,12 +97,8 @@ class ExecedProcess : public Process {
                    const int64_t stime_u);
 
   void initialize();
-  void propagate_file_usage(const FileName *name,
-                            const FileUsage* fu_change);
-  bool register_file_usage(const FileName *name, const FileName *actual_file,
-                           FileAction action, int flags, int error);
-  bool register_file_usage(const FileName *name, const FileUsage* fu_change);
-  bool register_parent_directory(const FileName *name, FileInitialState initial_state = ISDIR);
+  bool register_file_usage_update(const FileName *name, const FileUsageUpdate& update);
+  bool register_parent_directory(const FileName *name, FileType type = ISDIR);
   void add_pipe(std::shared_ptr<Pipe> pipe) {created_pipes_.insert(pipe);}
   std::vector<inherited_outgoing_pipe_t>& inherited_outgoing_pipes()
       {return inherited_outgoing_pipes_;}

src/firebuild/execed_process_cacher.cc
@@ -312,18 +312,13 @@ static void add_file(std::vector<FBBSTORE_Builder_file>* files, const FileName* 
   }
 }
 
-static void add_file_with_hash(std::vector<FBBSTORE_Builder_file>* files, const FileName* file_name,
-                               const Hash& content_hash, const int mode = -1) {
+static void add_file(std::vector<FBBSTORE_Builder_file>* files, const FileName* file_name,
+                     const Hash *content_hash, const int mode = -1) {
     FBBSTORE_Builder_file& new_file = files->emplace_back();
     fbbstore_builder_file_init(&new_file);
     fbbstore_builder_file_set_path_with_length(&new_file, file_name->c_str(), file_name->length());
-    fbbstore_builder_file_set_hash(&new_file, content_hash.get());
+    if (content_hash) fbbstore_builder_file_set_hash(&new_file, content_hash->get());
     if (mode != -1) fbbstore_builder_file_set_mode(&new_file, mode);
-}
-
-static void add_file_with_mode(std::vector<FBBSTORE_Builder_file>* files, const FileName* file_name,
-                               const int mode = -1) {
-  add_file_with_hash(files, file_name, Hash(), mode);
 }
 
 static const FBBSTORE_Builder* file_item_fn(int idx, const void *user_data) {
@@ -340,9 +335,8 @@ static bool dir_created_or_could_exist(
   while (parent_dir != nullptr) {
     const auto it = file_usages.find(parent_dir);
     const FileUsage* fu = it->second;
-    const FileInitialState initial_state = fu->initial_state();
-    if (initial_state == NOTEXIST || initial_state == NOTEXIST_OR_ISREG
-        || initial_state == NOTEXIST_OR_ISREG_EMPTY) {
+    if (fu->initial_type() == NOTEXIST || fu->initial_type() == NOTEXIST_OR_ISREG
+        || fu->initial_type() == NOTEXIST_OR_ISREG_EMPTY) {
       if (!fu->written()) {
         /* The process expects the directory to be missing but it does not create it.
          * This can't work. */
@@ -361,7 +355,7 @@ static bool dir_created_or_could_exist(
           return false;
         }
       }
-    } else if (fu->initial_state() == ISDIR) {
+    } else if (fu->initial_type() == ISDIR) {
       /* Directory is expected to exist. */
       return true;
     }
@@ -442,7 +436,7 @@ void ExecedProcessCacher::store(const ExecedProcess *proc) {
 
     /* If the file's initial contents matter, record it in pb's "inputs".
      * This is purely data conversion from one format to another. */
-    switch (fu->initial_state()) {
+    switch (fu->initial_type()) {
       case DONTKNOW:
         /* Nothing to do. */
         break;
@@ -481,10 +475,10 @@ void ExecedProcessCacher::store(const ExecedProcess *proc) {
     if (fu->written()) {
       int fd = open(filename->c_str(), O_RDONLY);
       if (fd >= 0) {
-        Hash new_hash;
         struct stat64 st;
         if (fstat64(fd, &st) == 0) {
           if (S_ISREG(st.st_mode)) {
+            Hash new_hash;
             /* TODO don't store and don't record if it was read with the same hash. */
             if (!hash_cache->store_and_get_hash(filename, &new_hash, fd, &st)) {
               /* unexpected error, now what? */
@@ -494,24 +488,24 @@ void ExecedProcessCacher::store(const ExecedProcess *proc) {
             }
             // TODO(egmont) fail if setuid/setgid/sticky is set
             int mode = st.st_mode & 07777;
-            add_file_with_hash(&out_path_isreg_with_hash, filename, new_hash, mode);
+            add_file(&out_path_isreg_with_hash, filename, &new_hash, mode);
           } else if (S_ISDIR(st.st_mode)) {
             // TODO(egmont) fail if setuid/setgid/sticky is set
             const int mode = st.st_mode & 07777;
-            add_file_with_mode(&out_path_isdir, filename, mode);
+            add_file(&out_path_isdir, filename, nullptr, mode);
             out_path_isdir_filename_ptrs.insert(filename);
           } else {
             // TODO(egmont) handle other types of entries
           }
         } else {
           perror("fstat");
-          if (fu->initial_state() != NOTEXIST) {
+          if (fu->initial_type() != NOTEXIST) {
             out_path_notexist.push_back(filename->c_str());
           }
         }
         close(fd);
       } else {
-        if (fu->initial_state() != NOTEXIST) {
+        if (fu->initial_type() != NOTEXIST) {
           out_path_notexist.push_back(filename->c_str());
         }
       }
@@ -852,8 +846,7 @@ const FBBSTORE_Serialized_process_inputs_outputs * ExecedProcessCacher::find_sho
  */
 static bool restore_dirs(
     ExecedProcess* proc,
-    const FBBSTORE_Serialized_process_outputs *outputs,
-    const FileUsage* fu) {
+    const FBBSTORE_Serialized_process_outputs *outputs) {
   /* Construct indices 0 .. path_isdir_count()-1 and initialize them with these values */
   std::vector<int> indices(fbbstore_serialized_process_outputs_get_path_isdir_count(outputs));
   size_t i;
@@ -897,7 +890,8 @@ static bool restore_dirs(
       return false;
     }
     if (proc->parent_exec_point()) {
-      proc->parent_exec_point()->propagate_file_usage(path, fu);
+      proc->parent_exec_point()->register_file_usage_update(
+          path, FileUsageUpdate(path, DONTKNOW, true));
     }
   }
   return true;
@@ -915,8 +909,7 @@ static bool restore_dirs(
  */
 static void remove_files_and_dirs(
     ExecedProcess* proc,
-    const FBBSTORE_Serialized_process_outputs *outputs,
-    const FileUsage* fu) {
+    const FBBSTORE_Serialized_process_outputs *outputs) {
   /* Construct indices 0 .. path_notexist_count()-1 and initialize them with these values */
   std::vector<int> indices(fbbstore_serialized_process_outputs_get_path_notexist_count(outputs));
   size_t i;
@@ -944,7 +937,8 @@ static void remove_files_and_dirs(
       rmdir(path->c_str());
     }
     if (proc->parent_exec_point()) {
-      proc->parent_exec_point()->propagate_file_usage(path, fu);
+      proc->parent_exec_point()->register_file_usage_update(
+          path, FileUsageUpdate(path, DONTKNOW, true));
     }
   }
 }
@@ -972,52 +966,47 @@ bool ExecedProcessCacher::apply_shortcut(ExecedProcess *proc,
           (fbbstore_serialized_process_inputs_get_path_isreg_at(inputs, i));
       const auto path = FileName::Get(fbbstore_serialized_file_get_path(file),
                                       fbbstore_serialized_file_get_path_len(file));
+      FileInfo info(ISREG);
       if (fbbstore_serialized_file_has_hash(file)) {
         Hash hash(fbbstore_serialized_file_get_hash(file));
-        const FileUsage* fu = FileUsage::Get(ISREG, hash);
-        proc->parent_exec_point()->propagate_file_usage(path, fu);
-      } else {
-        const FileUsage* fu = FileUsage::Get(ISREG);
-        proc->parent_exec_point()->propagate_file_usage(path, fu);
+        info.set_hash(hash);
       }
+      proc->parent_exec_point()->register_file_usage_update(path, FileUsageUpdate(path, info));
     }
     for (i = 0; i < fbbstore_serialized_process_inputs_get_path_isdir_count(inputs); i++) {
       const FBBSTORE_Serialized_file *file = reinterpret_cast<const FBBSTORE_Serialized_file *>
           (fbbstore_serialized_process_inputs_get_path_isdir_at(inputs, i));
       const auto path = FileName::Get(fbbstore_serialized_file_get_path(file),
                                       fbbstore_serialized_file_get_path_len(file));
+      FileInfo info(ISDIR);
       if (fbbstore_serialized_file_has_hash(file)) {
         Hash hash(fbbstore_serialized_file_get_hash(file));
-        const FileUsage* fu = FileUsage::Get(ISDIR, hash);
-        proc->parent_exec_point()->propagate_file_usage(path, fu);
-      } else {
-        const FileUsage* fu = FileUsage::Get(ISDIR);
-        proc->parent_exec_point()->propagate_file_usage(path, fu);
+        info.set_hash(hash);
       }
+      proc->parent_exec_point()->register_file_usage_update(path, FileUsageUpdate(path, info));
     }
     for (i = 0; i < fbbstore_serialized_process_inputs_get_path_notexist_or_isreg_count(inputs);
          i++) {
-      const FileUsage* fu = FileUsage::Get(NOTEXIST_OR_ISREG);
       const auto path = FileName::Get(
           fbbstore_serialized_process_inputs_get_path_notexist_or_isreg_at(inputs, i),
           fbbstore_serialized_process_inputs_get_path_notexist_or_isreg_len_at(inputs, i));
-      proc->parent_exec_point()->propagate_file_usage(path, fu);
+      proc->parent_exec_point()->register_file_usage_update(
+          path, FileUsageUpdate(path, NOTEXIST_OR_ISREG));
     }
     for (i = 0;
          i < fbbstore_serialized_process_inputs_get_path_notexist_or_isreg_empty_count(inputs);
          i++) {
-      const FileUsage* fu = FileUsage::Get(NOTEXIST_OR_ISREG_EMPTY);
       const auto path = FileName::Get(
           fbbstore_serialized_process_inputs_get_path_notexist_or_isreg_empty_at(inputs, i),
           fbbstore_serialized_process_inputs_get_path_notexist_or_isreg_empty_len_at(inputs, i));
-      proc->parent_exec_point()->propagate_file_usage(path, fu);
+      proc->parent_exec_point()->register_file_usage_update(
+          path, FileUsageUpdate(path, NOTEXIST_OR_ISREG_EMPTY));
     }
     for (i = 0; i < fbbstore_serialized_process_inputs_get_path_notexist_count(inputs); i++) {
-      const FileUsage* fu = FileUsage::Get(NOTEXIST);
       const auto path = FileName::Get(
           fbbstore_serialized_process_inputs_get_path_notexist_at(inputs, i),
           fbbstore_serialized_process_inputs_get_path_notexist_len_at(inputs, i));
-      proc->parent_exec_point()->propagate_file_usage(path, fu);
+      proc->parent_exec_point()->register_file_usage_update(path, FileUsageUpdate(path, NOTEXIST));
     }
   }
 
@@ -1025,10 +1014,7 @@ bool ExecedProcessCacher::apply_shortcut(ExecedProcess *proc,
       reinterpret_cast<const FBBSTORE_Serialized_process_outputs *>
       (fbbstore_serialized_process_inputs_outputs_get_outputs(inouts));
 
-  /* We'll reuse this for every file modification event to propagate. */
-  const FileUsage* fu = FileUsage::Get(DONTKNOW, true);
-
-  if (!restore_dirs(proc, outputs, fu)) {
+  if (!restore_dirs(proc, outputs)) {
     return false;
   }
 
@@ -1049,11 +1035,12 @@ bool ExecedProcessCacher::apply_shortcut(ExecedProcess *proc,
       chmod(path->c_str(), fbbstore_serialized_file_get_mode(file) & 0777);
     }
     if (proc->parent_exec_point()) {
-      proc->parent_exec_point()->propagate_file_usage(path, fu);
+      proc->parent_exec_point()->register_file_usage_update(
+          path, FileUsageUpdate(path, DONTKNOW, true));
     }
   }
 
-  remove_files_and_dirs(proc, outputs, fu);
+  remove_files_and_dirs(proc, outputs);
 
   /* See what the process originally wrote to its pipes. Add these to the Pipes' buffers. */
   for (i = 0; i < fbbstore_serialized_process_outputs_get_pipe_data_count(outputs); i++) {

src/firebuild/file_info.cc
@@ -1,0 +1,63 @@
+/* Copyright (c) 2022 Interri Kft. */
+/* This file is an unpublished work. All rights reserved. */
+
+/**
+ * FileInfo describes the (potentially partial) information that we know about a certain file, as it
+ * looked like / looks like / will look like in a certain point in time. It's up to the user of this
+ * structure to decide which point in time they refer to.
+ */
+
+#include "firebuild/file_info.h"
+
+#include "common/firebuild_common.h"
+#include "firebuild/debug.h"
+#include "firebuild/hash.h"
+#include "firebuild/hash_cache.h"
+
+namespace firebuild {
+
+bool operator==(const FileInfo& lhs, const FileInfo& rhs) {
+  return lhs.type_ == rhs.type_ &&
+         lhs.hash_known_ == rhs.hash_known_ &&
+         lhs.hash_ == rhs.hash_;
+}
+
+/* Global debugging methods.
+ * level is the nesting level of objects calling each other's d(), bigger means less info to print.
+ * See #431 for design and rationale. */
+std::string d(const FileInfo& fi, const int level) {
+  (void)level;  /* unused */
+  return std::string("{FileInfo type=") +
+      file_type_to_string(fi.type()) +
+      (fi.hash_known() ?
+          ", hash=" + d(fi.hash()) : "") + "}";
+}
+std::string d(const FileInfo *fi, const int level) {
+  if (fi) {
+    return d(*fi, level);
+  } else {
+    return "{FileInfo NULL}";
+  }
+}
+
+const char *file_type_to_string(FileType type) {
+  switch (type) {
+    case DONTKNOW:
+      return "dontknow";
+    case NOTEXIST:
+      return "notexist";
+    case NOTEXIST_OR_ISREG_EMPTY:
+      return "notexist_or_isreg_empty";
+    case NOTEXIST_OR_ISREG:
+      return "notexist_or_isreg";
+    case ISREG:
+      return "isreg";
+    case ISDIR:
+      return "isdir";
+    default:
+      assert(0 && "unknown type");
+      return "UNKNOWN";
+  }
+}
+
+}  /* namespace firebuild */

commit c0d6bb268f7efa07c5777637b6eab201224f9608

supervisor: Fix registering file usage update when report is generated
Seems to be a regression caused by the refactoring in
0d81e68698a80e1122626a21bae7d74b022b353d.

src/firebuild/execed_process.cc
@@ -303,7 +303,7 @@ bool ExecedProcess::register_file_usage_update(const FileName *name,
     if (!generate_report) {
       proc = proc->next_shortcutable_ancestor();
     } else {
-      proc = proc->exec_point();
+      proc = proc->parent_exec_point();
     }
   }
   return true;