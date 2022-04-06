firebuild/firebuild

commit a1f7a24c710f3f0a71691099a20918c5b5fec332

supervisor: Free up process data after storing in the cache
Fixes #271

src/firebuild/execed_process.cc
@@ -16,6 +16,7 @@
 #include "firebuild/utils.h"
 
 extern libconfig::Config * cfg;
+extern bool generate_report;
 
 namespace firebuild {
 
@@ -96,6 +97,18 @@ void ExecedProcess::do_finalize() {
   // store data for shortcutting
   if (cacher_ && !was_shortcut() && can_shortcut()) {
     cacher_->store(this);
+  }
+
+  // free up process data that we no longer need
+  for (auto& fu : file_usages()) {
+    delete fu.second;
+  }
+  file_usages().clear();
+  fds()->clear();
+  if (!generate_report) {
+    args().clear();
+    env_vars().clear();
+    libs().clear();
   }
 
   // Call the base class's method

src/firebuild/firebuild.cc
@@ -37,6 +37,7 @@
 
 /** global configuration */
 libconfig::Config * cfg;
+bool generate_report = false;
 
 namespace {
 
@@ -67,7 +68,6 @@ static int inherited_fd = -1;
 static int child_pid, child_ret = 1;
 static google::protobuf::io::FileOutputStream * error_fos;
 static bool insert_trace_markers = false;
-static bool generate_report = false;
 static const char *report_file = "firebuild-build-report.html";
 static firebuild::ProcessTree *proc_tree;
 static firebuild::ExecedProcessCacher *cacher;

src/firebuild/process.h
@@ -140,6 +140,7 @@ class Process {
       return nullptr;
     }
   }
+  std::shared_ptr<std::vector<std::shared_ptr<FileFD>>> fds() {return fds_;}
   void set_fds(std::shared_ptr<std::vector<std::shared_ptr<FileFD>>> fds) {fds_ = fds;}
   std::shared_ptr<std::vector<std::shared_ptr<FileFD>>> pass_on_fds(bool execed = true);
 

commit 2dfcdc123c1e4f05f263347217b2d2265abe6b25

supervisor: Fix listing used files in the report
Fixes regression introduced in a1f7a24c710f3f0a71691099a20918c5b5fec332 .

src/firebuild/execed_process.cc
@@ -176,10 +176,10 @@ void ExecedProcess::do_finalize() {
     cacher_->store(this);
   }
 
-  file_usages_.clear();
   fds()->clear();
   inherited_outgoing_pipes_.clear();
   if (!generate_report) {
+    file_usages_.clear();
     args().clear();
     env_vars().clear();
     libs_.clear();