120285a8f3c1978eb846ff2af1d4a6050edfa2d5
Balint Reczey <balint@balintreczey.hu>
2022-04-08 17:51:01 +0200

    interceptor: Push more logic to tpl_open.c merging tpl_openat.c
    Also use __OPEN_NEEDS_MODE to honor O_TMPFILE when checking flags.

--- src/interceptor/tpl_open.c
+++ src/interceptor/tpl_open.c
@@ -7,16 +7,36 @@
 {# ------------------------------------------------------------------ #}
 ### extends "tpl.c"
 
-{% set msg_add_fields = ["if (flags & O_CREAT) fbbcomm_builder_" + msg + "_set_mode(&ic_msg, mode);",
-                         "BUILDER_SET_ABSOLUTE_CANONICAL(" + msg + ", file);"] %}
+### if msg_add_fields is not defined
+###   if vararg
+###     set msg_add_fields = ["if (__OPEN_NEEDS_MODE(flags)) fbbcomm_builder_" + msg + "_set_mode(&ic_msg, mode);"]
+###   else
+###     set msg_add_fields = []
+###   endif
+###   if "dirfd" in sig_str
+###     do msg_add_fields.append("BUILDER_MAYBE_SET_ABSOLUTE_CANONICAL(" + msg + ", dirfd, file);")
+###   else
+###     do msg_add_fields.append("BUILDER_SET_ABSOLUTE_CANONICAL(" + msg + ", file);")
+###   endif
+### endif
+### set after_lines = ["if (i_am_intercepting) clear_notify_on_read_write_state(ret);"]
+### set send_ret_on_success=True
+### set ack_condition = "success && !is_path_at_locations(file, &system_locations)"
 
 ### block before
+{{ super() }}
+###   if vararg
   mode_t mode = 0;
-  if (flags & O_CREAT) {
+  if (__OPEN_NEEDS_MODE(flags)) {
     mode = va_arg(ap, mode_t);
   }
+###   endif
 ### endblock before
 
 ### block call_orig
+### if vararg
   ret = ic_orig_{{ func }}({{ names_str }}, mode);
+### else
+  ret = ic_orig_{{ func }}({{ names_str }});
+### endif
 ### endblock call_orig
