9840c2147afce3badcb1f083e4c5f4b36d9c4cfb
Balint Reczey <balint@balintreczey.hu>
2022-04-11 14:17:02 +0200

    interceptor: Send fork_parent message from pthread_atfork's parent handler
    Fixes #328 and #819.

--- src/interceptor/tpl_fork.c
+++ src/interceptor/tpl_fork.c
@@ -42,11 +42,8 @@
     /* Error, nothing here to do */
   } else if (ret == 0) {
     /* The child signed in to the supervisor in atfork_child_handler(), nothing else here to do. */
-  } else if (i_am_intercepting) {
-    /* Parent */
-    FBBCOMM_Builder_fork_parent ic_msg;
-    fbbcomm_builder_fork_parent_init(&ic_msg);
-    fb_fbbcomm_send_msg_and_check_ack(&ic_msg, fb_sv_conn);
+  } else {
+    /* Parent sends the fork_parent message in atfork_parent_handler(). */
   }
 
   /* Common for all three outcomes: re-enable signal delivery */
