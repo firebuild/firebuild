26402034ecc68d7a2132c0b6712c394ccccff022
Balint Reczey <balint@balintreczey.hu>
2022-12-21 16:58:02 +0100

    Update copyright in source files to match LICENSE.md

--- src/common/cstring_view.h
+++ src/common/cstring_view.h
@@ -1,8 +1,19 @@
 /*
  * Copyright (c) 2022 Firebuild Inc.
  * All rights reserved.
+ *
  * Free for personal use and commercial trial.
  * Non-trial commercial use requires licenses available from https://firebuild.com.
+ * Modification and redistribution are permitted, but commercial use of derivative
+ * works is subject to the same requirements of this license
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #ifndef COMMON_CSTRING_VIEW_H_
--- src/interceptor/tpl_recvmsg.c
+++ src/interceptor/tpl_recvmsg.c
@@ -3,7 +3,18 @@
 {# All rights reserved.                                               #}
 {# Free for personal use and commercial trial.                        #}
 {# Non-trial commercial use requires licenses available from          #}
-{# https://firebuild.com                                              #}
+{# https://firebuild.com.                                             #}
+{# Modification and redistribution are permitted, but commercial use  #}
+{# of derivative works is subject to the same requirements of this    #}
+{# license.                                                           #}
+{# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,    #}
+{# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF #}
+{# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND              #}
+{# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT        #}
+{# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,       #}
+{# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, #}
+{# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER      #}
+{# DEALINGS IN THE SOFTWARE.                                          #}
 {# ------------------------------------------------------------------ #}
 {# Template for the recv[m]msg() calls.                               #}
 {# In addition to the "read" template, i.e. notifying the supervisor  #}
